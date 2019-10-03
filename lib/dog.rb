e2007461ab97e42ad512470abcce811cfe9a6b43
Amgad Malik <amgadmalik@yahoo.com>
2019-10-03 01:09:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+class Dog 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    
+    save
+     
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.print_all
+    @@all.each do |el|
+     puts "#{el.name}" 
+    end
+    #binding.pry
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  #private 
+    def save
+    @@all << self
+    end
+ 
+end 
